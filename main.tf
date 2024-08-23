27218cc6cd7afde85f9b7d62fe5f965fbf18dd25
Martin <mbergstrand@hagerty.com>
2024-08-23 11:44:54 -0500

    load balancer

--- main.tf
+++ main.tf
@@ -37,18 +37,54 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id               = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids  = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  listeners = {
+    {
+      port     = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  }
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+        target_id = aws_instance.blog
+        port      = 80
+      }
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+    Project     = "temp"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
+  
   name    = "blog"
 
   vpc_id = module.blog_vpc.vpc_id
