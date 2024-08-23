1f21e349983e50aceb05eeef80f170fc6d5e7e5d
Martin <mbergstrand@hagerty.com>
2024-08-23 14:48:36 -0500

    tg

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "blog_asg" {
   max_size = 2
 
   vpc_zone_identifier                 = module.blog_vpc.public_subnets
-  autoscaling_group_target_group_arns = module.blog_alb.target_group_arns
+  # autoscaling_group_target_group_arns = module.blog_alb.target_group_arns
   security_groups                     = [module.blog_sg.security_group_id]
   
   image_id      = data.aws_ami.app_ami.id
@@ -67,6 +67,8 @@ module "blog_alb" {
       }
     }
   }
+
+  target_group_arns = autoscaling_group_target_group_arns
 }
 
 
