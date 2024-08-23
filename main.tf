f95bd22a1e0577854ea44329e11c2612e3e33379
Martin <mbergstrand@hagerty.com>
2024-08-23 16:11:50 -0500

    whoops

--- main.tf
+++ main.tf
@@ -14,10 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -26,11 +22,6 @@ module "blog_vpc" {
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  traffic_source_attachments = {
-    traffic_source_identifier = module.blog_alb.id
-    traffic_source_type       = "elbv2"
-  }
 
   tags = {
     Terraform = "true"
@@ -46,11 +37,15 @@ module "blog_asg" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_groups
   security_groups     = [module.blog_sg.security_group_id]
   
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  traffic_source_attachments = {
+    traffic_source_identifier = module.blog_alb.id
+    traffic_source_type       = "elbv2"
+  }
 }
 
 module "blog_alb" {
